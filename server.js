e3a805b3de6e8863adb56efb1d3a68b5977aa635
Nitesh <ext_nkhanduja@falabella.cl>
2022-04-17 12:51:12 +0530

    helmet morgan colors added

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose')
 const userRouter  = require('./Routers/userRouters')
 const shopRouter  = require('./Routers/shopRoutes')
+const morgan = require('morgan');
+const helmet = require("helmet");
+const colors = require('colors');
 
 //db connection
 require('dotenv/config')
@@ -10,18 +13,23 @@ mongoose.connect(process.env.MONGODB_URL_LOCAL,{
     useUnifiedTopology:true,
     // useCreateIndex:true
 })
-.then (()=>console.log("Conneted to Mongo db"))
+.then (()=>console.log(colors.bgGreen(" 💯👍 Conneted to Mongo db ")))
 .catch((err)=> console.log("Connection Failed",err))
 
 
 const app = express();
 // parse
 app.use(express.json());
+app.use(helmet());
 
 app.use('/api/user',userRouter)
 app.use('/api/shop',shopRouter)
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
 
 const port  = process.env.PORT || 8080;
 app.listen(port,()=>{
-    console.log(`App running on port ${port} `)
+    console.log(colors.green(`😁App running on port ${port} `))
 })
